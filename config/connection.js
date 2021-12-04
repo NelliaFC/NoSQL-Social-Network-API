0bf580b5ef7d0ba1e260d758d637ef51a4be717a
NeoNeonilla <nelliafleurova@gmail.com>
2021-12-04 14:06:57 -0600

    connection update

--- config/connection.js
+++ config/connection.js
@@ -3,8 +3,10 @@ const mongoose = require('mongoose');
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/socialmedia', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
+
 });
 
+
+// Use this to log mongo queries being executed!
+mongoose.set('debug', true);
 module.exports = mongoose.connection;
