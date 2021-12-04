37df212a44e923c85fcd99c51a6bac413b791f62
NeoNeonilla <nelliafleurova@gmail.com>
2021-12-04 14:21:22 -0600

    troubleshooting connection

---
+++ server.js
@@ -0,0 +1,7 @@
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/pizza-hunt', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+// Use this to log mongo queries being executed!
+mongoose.set('debug', true);
